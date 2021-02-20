0a6e62a3b48213737857744a6915e57a85a58d02
Alana Soares Reis <72468925+lanasreis@users.noreply.github.com>
2021-02-20 11:01:12 -0300

    Comportamento básico, dinamica ruim

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Sidebar from "./components/Sidebar";
+import Video from './components/Video';
 
 function App() {
   return (
     <div className="App">
-      
+      <Video />
+      <Sidebar />
+
     </div>
   );
 }
